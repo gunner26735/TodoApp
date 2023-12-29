2412dff6d2dde2bbce1a5cb67b5a925250473748
gunner26735 <sidshukla2016@gmail.com>
2023-12-30 00:43:44 +0530

    created test cases for Todo Details , List

--- todo_main/tests.py
+++ todo_main/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
-from . import serializer,views
+from django.http import Http404
+from rest_framework.test import APITestCase,APIClient
+from rest_framework import status
+from . import serializer,views,models
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 
@@ -45,3 +48,144 @@ class TestUserView(TestCase):
     def test_400post(self):
         response = self.client.post('/todo/register/',data=self.err_user_data,format='json')
         self.assertEqual(response.status_code,400)
+
+class TodoListTestCase(TestCase):
+    def setUp(self):
+        #creating user for Token
+        self.user_data = {
+            'username': 'hello',
+            'password': 'hello123'
+        }
+
+        # create some test data
+        self.err_data = {
+            "title": "POST TEST CASE",
+            "description": "Create a POST TEST CASE",
+            "due_date": "2023-12-31",
+            "status": "WORKEDD" #err data
+        }
+        self.data = {
+            "title": "POST TEST CASE",
+            "description": "Create a POST TEST CASE",
+            "due_date": "2023-12-31",
+            "status": "WORKING"
+        }
+        models.todo.objects.create(title = "Test1",
+            description= "Create a Test case",
+            due_date= "2023-12-31",
+            status= "OPEN"
+        )
+
+    def get_token(self):
+        self.client.post('/todo/register/',data=self.user_data,format='json')
+        #get token obj
+        tuser = User.objects.get(username=self.user_data['username'])
+        return(Token.objects.get(user=tuser))
+
+    def test_get_todo_list(self):
+        token = self.get_token()
+        # create a client with the token
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION=str('Token ' + str(token)))
+        # get the response from the view
+        response = client.get('/todo/')
+        # get the data from the db
+        todos = models.todo.objects.all()
+        serial = serializer.TodoSerializer(todos, many=True)
+        # compare the two
+        self.assertEqual(response.data, serial.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post_todo_list(self):      
+        token = self.get_token()
+        # create a client with the token
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION=str('Token ' + str(token)))
+        # get the API response
+        response = client.post("/todo/", self.data)
+        # get the data from the db
+        todo = models.todo.objects.get(title="POST TEST CASE")
+        serial = serializer.TodoSerializer(todo)
+        # compare the two
+        self.assertEqual(response.data, serial.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_400post_todo(self):
+        
+        token = self.get_token()
+        # create a client with the token
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION=str('Token ' + str(token)))
+        # get the API response
+        response = client.post("/todo/", self.err_data)
+        # get the data from the db
+        self.assertEqual(response.status_code,400)
+
+
+class TodoTodoDetailTestCases(TestCase):
+    def setUp(self):
+        #created a todo
+        self.test1 = models.todo.objects.create(
+            title = "Test1",
+            description= "Create a Test case",
+            due_date= "2023-12-31",
+            status= "WORKING"
+        )
+
+        #creating user for Token
+        self.user_data = {
+            'username': 'hello',
+            'password': 'hello123'
+        }
+
+        token = self.get_token()
+        # create a client with the token
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION=str('Token ' + str(token)))
+
+    def get_token(self):
+        self.client.post('/todo/register/',data=self.user_data,format='json')
+        #get token obj
+        tuser = User.objects.get(username=self.user_data['username'])
+        return(Token.objects.get(user=tuser))
+    
+    def test_get_object(self):
+        self.res = views.TodoDetail.get_object(self,pk=self.test1.pk)
+        self.assertEqual(self.res,self.test1)
+        #For error check
+        with self.assertRaises(Http404):
+            views.TodoDetail.get_object(self,pk=99)
+
+    def test_get(self):
+        res = self.client.get(f'/todo/{self.test1.pk}/')
+        self.assertEqual(res.status_code,status.HTTP_200_OK)
+        self.assertEqual(res.data,(serializer.TodoSerializer(self.test1).data))
+
+    def test_update(self):
+        updated_data = {
+            "title": "Test1_2",
+            "description": "Create a Test case",
+            "due_date": "2024-01-01",
+            "status": "DONE"}
+        response = self.client.put(f'/todo/{self.test1.id}/', updated_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.test1.refresh_from_db()
+        self.assertEqual(self.test1.title, updated_data['title'])
+        self.assertEqual(self.test1.description, updated_data['description'])
+        
+
+        updated_err_data = {
+            "topic": "Test1_2",
+            "description": "Create a Test case",
+            "due_date": "2024-01-01",
+            "status": "DONE"}
+        response2 = self.client.put(f'/todo/{self.test1.id}/', updated_err_data, format='json')
+        self.assertEqual(response2.status_code,status.HTTP_400_BAD_REQUEST)
+
+    def test_delete(self):
+        response = self.client.delete(f'/todo/{self.test1.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        with self.assertRaises(models.todo.DoesNotExist):
+            self.test1.refresh_from_db()
